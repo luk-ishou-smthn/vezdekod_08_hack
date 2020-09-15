c71a5d913153b0ff8e1ba9bb2c89b15e05da169b
vldplcd <shilov.part@gmail.com>
2020-09-15 09:41:57 +0300

    Updates

---
+++ precache-manifest.add5513ce44e80541b38d10efac07a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff346ef3258eb4a53ad18067bea551e3",
+    "url": "/vezdekod_08_hack/index.html"
+  },
+  {
+    "revision": "76fafa9a76c719544550",
+    "url": "/vezdekod_08_hack/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "76fafa9a76c719544550",
+    "url": "/vezdekod_08_hack/static/js/2.4754b04a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vezdekod_08_hack/static/js/2.4754b04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d698e4eb1c75e9e48f35",
+    "url": "/vezdekod_08_hack/static/js/main.870669f1.chunk.js"
+  },
+  {
+    "revision": "157f3fffc976b94ece03",
+    "url": "/vezdekod_08_hack/static/js/runtime-main.135a4e29.js"
+  }
+]);
